4c8ffa9a7646eca269b470ecfba9d8f3da43f71d
Pavel <petrovengineer@gmail.com>
2020-08-31 11:58:00 +0300

    multi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Select from './Select';
 
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
 function App() {
+  const [selected, setSelected] = useState([]);
+  const [selected2, setSelected2] = useState(null);
   return (
     <div className="App">
-      <Select/>
+      <Select options={options} selected={selected} setSelected={setSelected} multi/>
+      <Select options={options} selected={selected2} setSelected={setSelected2}/>
     </div>
   );
 }
--- src/Select/index.js
+++ src/Select/index.js
@@ -1,29 +1,53 @@
 import React, { useState } from 'react';
-import Select from 'react-select';
 import './style.css';
+import {ReactComponent as Arrow} from '../svg/next.svg';
 
-const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
-
-export const SelectWrapper = (props)=>{
-    const [selected, setSelected] = useState('chocolate');
+export const SelectWrapper = ({options, multi, selected=multi?[]:null, setSelected})=>{
     const [select, showSelect] = useState(false);
     return (
         <div style={{display:'inline-block'}}>
-            <div className={select?'bg':''} onClick={()=>showSelect(false)}></div>
-            <div className={['btn', select?'btn-selected':'btn-hover'].join(' ')} onClick={()=>showSelect(!select)}>
-                <div className="btn-text">{selected?options.find(o=>selected==o.value).label:'Select'}</div>
+            <div className={['btn', select?'btn-selected':'btn-hover'].join(' ')} 
+                onClick={(e)=>{
+                    if(e.target.id!=='btn-close'){showSelect(!select)};
+                }}>
+                {multi?<div className="btn-text">{selected.map((s,i)=>{
+                    return <span className="btn-item" key={'s'+i}>
+                        {s}
+                        <span className="btn-item-close" id="btn-close" onClick={()=>{
+                            const newSelected = [...selected];
+                            newSelected.splice(i,1);
+                            setSelected(newSelected);
+                        }}>x</span>
+                    </span>
+                })}{selected.length===0?'Выбрать':null}</div>
+                :<div className="btn-text">{selected?options.find(o=>selected===o.value).label:'Выбрать'}</div>
+                }
+                <div className="btn-right">
+                    <span className="btn-separator"></span>
+                        <Arrow className="btn-arrow"/>
+                </div>
             </div>
-                {select?<div className="items">
-                    {options.map((o, i)=>{
-                        return o.value!=selected?
-                            <div key={'k'+i} className="item" onClick={()=>{setSelected(o.value); showSelect(false);}}>{o.label}</div>
-                            :null
-                    })}
-                </div>:null}
+{/* ==========================ITEMS======================================================= */}
+            {select?<div className="items">{
+                multi?options.filter(o=>selected.indexOf(o.value)>-1).length!==options.length?
+                        options.map((o, i)=>(selected.indexOf(o.value)===-1?
+                            <div key={'k'+i} className="item" 
+                                onClick={()=>{setSelected([...selected, o.value]);}}>
+                                {o.label}
+                            </div>
+                        :null))
+                    :<div className="item" style={{color:'gray'}}>Пусто</div>
+                :options.length!=0?options.map((o, i)=>(
+                    o.value!==selected?
+                        <div key={'k'+i} className="item" onClick={()=>{showSelect(false); setSelected(o.value);}}>
+                                {o.label}
+                        </div>
+                    :null
+                )):<div className="item" style={{color:'gray'}}>Пусто</div>
+                }</div>
+                :null
+            }
+            <div className={select?'bg':''} onClick={()=>showSelect(false)}></div>
         </div>
     )
 }
